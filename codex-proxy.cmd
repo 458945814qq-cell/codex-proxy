ee9ac7a1891711722b270411d4c6b26ee397b4b5
458945814qq-cell <458945814qq@gmail.com>
2026-09-13 07:18:52 +0800

    Update codex-proxy.cmd

--- codex-proxy.cmd
+++ codex-proxy.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-set "HTTP_PROXY=http://127.0.0.1:7897"
-set "HTTPS_PROXY=http://127.0.0.1:7897"
+set "HTTP_PROXY=http://127.0.0.1:443"
+set "HTTPS_PROXY=http://127.0.0.1:443"
 set "NO_PROXY=localhost,127.0.0.1,::1"
 set "ALL_PROXY="
 
